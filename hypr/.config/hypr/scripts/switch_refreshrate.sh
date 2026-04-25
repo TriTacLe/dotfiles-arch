caaa37b20ae5e0be4cee32524e4c2f70d36157a1
Tri Tac Le <trile@stud.ntnu.no>
2026-04-25 23:43:36 +0200

    CRITICAL: Disable switch_refreshrate.sh active code - Production PC safe!

--- hypr/.config/hypr/scripts/switch_refreshrate.sh
+++ hypr/.config/hypr/scripts/switch_refreshrate.sh
@@ -13,13 +13,13 @@ fi
 STATE=$(cat "$STATE_FILE")
 
 if [ "$STATE" = "60" ]; then
-    hyprctl keyword monitor "eDP-1, 1920x1200@120, 0x0"
+#    hyprctl keyword monitor "eDP-1, 1920x1200@120, 0x0"
     notify-send "Set refreshrate to 120hz"
 
     echo "120" > "$STATE_FILE"
 
 else
-    hyprctl keyword monitor "eDP-1, 1920x1200@60, 0x0"
+#    hyprctl keyword monitor "eDP-1, 1920x1200@60, 0x0"
     notify-send "Set refreshrate to 60hz"
 
     echo "60" > "$STATE_FILE"
